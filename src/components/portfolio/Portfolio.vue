860f2ff88b9b0927ccfb267846297b114997b4d9
Victor Nascimento <victor.nascimento.ns@gmail.com>
2022-05-03 21:00:56 -0300

    show active projects in slider home

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -5,21 +5,21 @@
       v-bind="settings"
       class="rn-slick-activation portfolio-slick-activation item-fluid slick-dotted rn-slick-dot mt--40 mt_sm--40"
     >
-      <!-- Start Single Portfolio  -->
+      <!-- Start Single Portfolio (thumbnail: 550x650px)  -->
       <div class="portfolio" v-for="(item, i) in portfolioContent" :key="i">
         <div class="thumbnail-inner">
-          <v-img class="thumbnail" :src="item.src"></v-img>
-          <v-img class="bg-blr-image" :src="item.src"></v-img>
+          <v-img class="thumbnail" :src="item.thumb"></v-img>
+          <v-img class="bg-blr-image" :src="item.thumb"></v-img>
         </div>
         <div class="content">
           <div class="inner">
             <p>{{ item.meta }}</p>
             <h4>
-              <router-link to="portfolio-details">{{ item.title }}</router-link>
+              <router-link :to="`/projeto/${item.slug}`">{{ item.title }}</router-link>
             </h4>
             <div class="portfolio-button">
-              <router-link class="rn-btn" to="/portfolio-details"
-                >Case Study</router-link
+              <router-link class="rn-btn" :to="`/projeto/${item.slug}`"
+                >Ver Projeto</router-link
               >
             </div>
           </div>
@@ -38,48 +38,6 @@
     },
     data() {
       return {
-        portfolioContent: [
-          {
-            src: require("../../assets/images/projects/constelacao-dvl/thumb-390x532.jpg"),
-            meta: "Development",
-            title: "Getting tickets to the big show",
-          },
-          {
-            src: require("../../assets/images/projects/constelacao-dvl/thumb-390x532.jpg"),
-            meta: "Development",
-            title: "Getting tickets to the big show",
-          },
-          {
-            src: require("../../assets/images/projects/constelacao-dvl/thumb-390x532.jpg"),
-            meta: "Development",
-            title: "Getting tickets to the big show",
-          },
-          {
-            src: require("../../assets/images/projects/constelacao-dvl/thumb-390x532.jpg"),
-            meta: "Development",
-            title: "Getting tickets to the big show",
-          },
-          {
-            src: require("../../assets/images/projects/constelacao-dvl/thumb-390x532.jpg"),
-            meta: "Development",
-            title: "Getting tickets to the big show",
-          },
-          {
-            src: require("../../assets/images/projects/constelacao-dvl/thumb-390x532.jpg"),
-            meta: "Development",
-            title: "Getting tickets to the big show",
-          },
-          {
-            src: require("../../assets/images/projects/constelacao-dvl/thumb-390x532.jpg"),
-            meta: "Development",
-            title: "Getting tickets to the big show",
-          },
-          {
-            src: require("../../assets/images/projects/constelacao-dvl/thumb-390x532.jpg"),
-            meta: "Development",
-            title: "Getting tickets to the big show",
-          },
-        ],
         // for all works
         settings: {
           dots: true,
@@ -137,5 +95,10 @@
         },
       };
     },
+    computed: {
+      portfolioContent () {
+        return this.$store.state.projects.filter(project => project.showHome)
+      }
+    }
   };
 </script>
